fb2d9ebfc74631e1278eab06c35f8de8453e67da
nuhuxi <mark.keckeis@gmail.com>
2014-02-26 15:58:04 -0700

    Make input work

---
+++ js/shopping-list.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+// these statements tell us the "state" of the user
+    $("#addbutton").mousedown(function() {
+      var addthisitem = $("additeminput").val();
+      console.log(addthisitem);
+    })
+
+  });
